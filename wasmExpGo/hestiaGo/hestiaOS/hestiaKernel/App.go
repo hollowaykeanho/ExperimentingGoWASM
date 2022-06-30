6a138a3b00bdfb4802224e0f94d2332f4332606f
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2022-06-30 17:08:17 +0800

    wasmExpGo/hestiaGo/hestiaOS: added hestiaKernel package
    Since WASM itself currently works like a single thread with no
    goroutine concurrency, there is a need to add its own app kernel
    structure for its operating system self-control purposes. Hence,
    let's add one in based on mobile application app lifecycle approach.
    
    This pathc adds hestiaKernel package with app lifecycle kernel
    approach into wasmExpGo/hestiaGo/hestiaOS/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ wasmExpGo/hestiaGo/hestiaOS/hestiaKernel/App.go
@@ -0,0 +1,157 @@
+// Copyright 2022 "Holloway" Chew, Kean Ho <hollowaykeanho@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package hestiaKernel
+
+import (
+	"hestiaGo/hestiaError"
+	"hestiaGo/hestiaOS"
+)
+
+// App is the app activity life-cycle approach kernel structure.
+//
+// The design was based on iOS and Android mobile application life-cycle
+// approach. This kernel structure focused on event-driven signalling for
+// triggering based-level activities either on its own (OS-independent) or
+// interacting with the opreating system signals like power management and
+// interrupts.
+//
+// This kernel structure also respects operating system's signals like SIGINT,
+// SIGTERM, and SIGKILL shall stop and destroy the executing app.
+type App struct {
+	// OnCreate is for initializing all resources ready to use.
+	//
+	// It accepts a `State` parameter which can be any data type that
+	// contains the saved state of the App.
+	//
+	// This function is **OPTIONAL**.
+	OnCreate func(interface{})
+
+	// OnDestroy is for exiting the application.
+	//
+	// This function shall destroy the application, freeing its own
+	// resources and conclude the app executions.
+	//
+	// This function is **OPTIONAL**.
+	OnDestroy func()
+
+	// OnPause is for handling for pausing or entering power sleep state.
+	//
+	// This function shall prepare the application for temporary acitvity
+	// halting or power sleep state where its preparations is for later
+	// restorations.
+	OnPause func()
+
+	// OnRestart is for any restart executions prior to a `Start` function.
+	//
+	// The function is designed solely for early interaction for a heavy
+	// and long waiting `Start` function.
+	//
+	// This function is **OPTIONAL**.
+	OnRestart func()
+
+	// SignalRestart is the interrupt value for executing OnRestart().
+	//
+	// To enable such signal, it **SHALL NOT** be zero or conflicting with
+	// all defined hestiaOS.SIGNAL values.
+	SignalRestart uint16
+
+	// OnResume is for returning back from paused or power slept state.
+	//
+	// This function shall restore the application back to operational
+	// mode.
+	OnResume func()
+
+	// OnStart is for starting the Application.
+	//
+	// This function shall start the application executions.
+	//
+	// This function is **COMPULSORY**.
+	OnStart func()
+
+	// OnStop is for stopping the Application.
+	//
+	// This function shall stop the application but not destroying it. At
+	// this state, it can be started again.
+	//
+	// This function is **COMPULSORY**.
+	OnStop func()
+
+	signaler *hestiaOS.Signal
+}
+
+// AppKill signals a running app to stop running and destroy itself.
+func AppKill(app *App) (err hestiaError.Error) {
+	return _appKill(app)
+}
+
+// AppPause signals a running app to enter its pause state.
+func AppPause(app *App) (err hestiaError.Error) {
+	return _appPause(app)
+}
+
+// AppRestart signals a running app to restart itself.
+func AppRestart(app *App) (err hestiaError.Error) {
+	return _appRestart(app)
+}
+
+// AppResume signals a paused app to its running state.
+func AppResume(app *App) (err hestiaError.Error) {
+	return _appResume(app)
+}
+
+// AppRun executes a given app from create to destroy.
+func AppRun(app *App, state interface{}) (err hestiaError.Error) {
+	return _appRun(app, state)
+}
+
+func appSignal(app *App, value uint16) (err hestiaError.Error) {
+	err = appValidate(app)
+	if err != hestiaError.OK {
+		return err
+	}
+
+	if app.signaler == nil {
+		return hestiaError.ENOPROTOOPT
+	}
+
+	if value == 0x00 {
+		return hestiaError.ENOMSG
+	}
+
+	hestiaOS.SignalSend(app.signaler, value)
+	return hestiaError.OK
+}
+
+func appValidate(app *App) (err hestiaError.Error) {
+	if app == nil {
+		return hestiaError.ENOENT
+	}
+
+	if app.OnStop == nil {
+		return hestiaError.ENOPROTOOPT
+	}
+
+	if app.OnStart == nil {
+		return hestiaError.ENOPROTOOPT
+	}
+
+	return hestiaError.OK
+}
---
+++ wasmExpGo/hestiaGo/hestiaOS/hestiaKernel/app_all.go
@@ -0,0 +1,109 @@
+// Copyright 2022 "Holloway" Chew, Kean Ho <hollowaykeanho@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+//go:build !wasm
+// +build !wasm
+
+package hestiaKernel
+
+import (
+	"hestiaGo/hestiaError"
+	"hestiaGo/hestiaOS"
+)
+
+func _appRun(app *App, state interface{}) (err hestiaError.Error) {
+	var signal uint16
+
+	// validate all inputs
+	err = appValidate(app)
+	if err != hestiaError.OK {
+		return err
+	}
+
+	// initialize key variables
+	app.signaler = &hestiaOS.Signal{}
+	err = hestiaOS.SignalInit(app.signaler)
+	if err != hestiaError.OK {
+		return err
+	}
+
+	// create the app environment first
+	if app.OnCreate != nil {
+		app.OnCreate(state)
+	}
+
+	// start the app
+	app.OnStart()
+
+	// wait for interrupt signal
+	for {
+		signal = hestiaOS.SignalWait(app.signaler)
+
+		switch {
+		case signal == 0x00:
+			continue
+		case signal == app.SignalRestart:
+			if app.OnRestart != nil {
+				app.OnRestart()
+			}
+			app.OnStop()
+			app.OnStart()
+		case signal == hestiaOS.SIGNAL_SIGSTOP:
+			if app.OnPause != nil {
+				app.OnPause()
+			}
+		case signal == hestiaOS.SIGNAL_SIGCONT:
+			if app.OnResume != nil {
+				app.OnResume()
+			}
+		case signal == hestiaOS.SIGNAL_SIGINT:
+			goto end
+		case signal == hestiaOS.SIGNAL_SIGTERM:
+			goto end
+		case signal == hestiaOS.SIGNAL_SIGKILL:
+			goto end
+		default:
+		}
+	}
+
+end:
+	app.OnStop()
+	if app.OnDestroy != nil {
+		app.OnDestroy()
+	}
+
+	return hestiaError.OK
+}
+
+func _appKill(app *App) (err hestiaError.Error) {
+	return appSignal(app, hestiaOS.SIGNAL_SIGKILL)
+}
+
+func _appPause(app *App) (err hestiaError.Error) {
+	return appSignal(app, hestiaOS.SIGNAL_SIGSTOP)
+}
+
+func _appRestart(app *App) (err hestiaError.Error) {
+	return appSignal(app, app.SignalRestart)
+}
+
+func _appResume(app *App) (err hestiaError.Error) {
+	return appSignal(app, hestiaOS.SIGNAL_SIGCONT)
+}
